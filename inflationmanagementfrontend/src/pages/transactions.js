00481af459a074431fd575b361cc343270099c02
Antoine Ndiaye <antoinendiaye09@gmail.com>
2023-06-07 21:33:37 +0200

    feat:IM-23 ajout de la deuxieme barre orange a la page de transactions

--- inflationmanagementfrontend/src/pages/transactions.js
+++ inflationmanagementfrontend/src/pages/transactions.js
@@ -1,10 +1,12 @@
 import BarreNavigationComponent from '../components/barreNavigation.component';
+import BarreTransactionComponent from '../components/barreTransaction.component';
 import '../styles/accueil.page.css';
 
 export default function TransactionPage(){
     return(
         <div className="page_accueil">
             <BarreNavigationComponent/>
+            <BarreTransactionComponent/>
         </div>
     );
 }
