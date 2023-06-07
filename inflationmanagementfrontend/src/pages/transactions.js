a69df05c5668a6fb47126e9a6f1e0928c41da5e0
Antoine Ndiaye <antoinendiaye09@gmail.com>
2023-06-07 15:10:41 +0200

    feat:IM-23 creation du fichier pour la page des transactions

---
+++ inflationmanagementfrontend/src/pages/transactions.js
@@ -0,0 +1,10 @@
+import BarreNavigationComponent from '../components/barreNavigation.component';
+import '../styles/accueil.page.css';
+
+export default function TransactionPage(){
+    return(
+        <div className="page_accueil">
+            <BarreNavigationComponent/>
+        </div>
+    );
+}
