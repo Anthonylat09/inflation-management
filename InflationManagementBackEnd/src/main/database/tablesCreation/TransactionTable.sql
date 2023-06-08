e333f6e54b25fc61b0d73993d94054abffc6ff4c
sonko <befatou25@gmail.com>
2023-06-08 11:25:08 +0200

    refactor: mise a jour des scripts de creation des tables

---
+++ InflationManagementBackEnd/src/main/database/tablesCreation/TransactionTable.sql
@@ -0,0 +1,11 @@
+USE budgetbd; #mettre le nom de votre bd
+DROP TABLE IF EXISTS Transaction;
+CREATE TABLE Transaction (
+                             id_transaction BIGINT PRIMARY KEY AUTO_INCREMENT,
+                             nom_transaction VARCHAR(255) NOT NULL,
+                             est_revenu TINYINT(1) NOT NULL,
+                             montant DOUBLE NOT NULL,
+                             categorie BIGINT NOT NULL,
+                             date DATE NOT NULL,
+                             FOREIGN KEY (categorie) REFERENCES Category(id_categorie)
+);
