fafb09c88f0d3bf0a3ca3c3326b0b10fa88f2fc0
sonko <befatou25@gmail.com>
2023-06-08 11:29:33 +0200

    fix: mise a jour des entites et services

--- InflationManagementBackEnd/src/main/java/com/services/UserService.java
+++ InflationManagementBackEnd/src/main/java/com/services/UserService.java
@@ -4,6 +4,8 @@ import com.entities.User;
 import com.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -12,11 +14,45 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User saveUser(User user) {
+    public User createUser(User user) {
         return userRepository.save(user);
     }
 
-    public User findUserById(Long id) {
+    public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            // Mise à jour des attributs de l'utilisateur existant avec les valeurs de l'utilisateur mis à jour
+            user.setNom(updatedUser.getNom());
+            user.setPrenom(updatedUser.getPrenom());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+            user.setBudget(updatedUser.getBudget());
+            user.setRole(updatedUser.getRole());
+
+            return userRepository.save(user);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
