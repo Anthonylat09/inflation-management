b273bc10fc4d1970116d693cdc58b1409a9ce5e7
sonko <befatou25@gmail.com>
2023-06-08 11:35:13 +0200

    feat: ajout des controllers

---
+++ InflationManagementBackEnd/src/main/java/com/controllers/TransactionController.java
@@ -0,0 +1,57 @@
+package com.controllers;
+
+import com.entities.Transaction;
+import com.services.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        return ResponseEntity.ok(transactions);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
+        Transaction createdTransaction = transactionService.createTransaction(transaction);
+        return ResponseEntity.ok(createdTransaction);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable long id) {
+        Transaction transaction = transactionService.getTransactionById(id);
+        if (transaction != null) {
+            return ResponseEntity.ok(transaction);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Transaction> updateTransaction(@PathVariable long id, @RequestBody Transaction updatedTransaction) {
+        Transaction updated = transactionService.updateTransaction(id, updatedTransaction);
+        if (updated != null) {
+            return ResponseEntity.ok(updated);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable long id) {
+        boolean deleted = transactionService.deleteTransaction(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
