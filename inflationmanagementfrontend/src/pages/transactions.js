019f71c46871051ee25d16a6ef10aa4617c7c5e7
Antoine Ndiaye <antoinendiaye09@gmail.com>
2023-06-13 16:53:36 +0200

    feat:IM-33 ajout du code de base pour recuperer les transactions du backend

--- inflationmanagementfrontend/src/pages/transactions.js
+++ inflationmanagementfrontend/src/pages/transactions.js
@@ -1,12 +1,24 @@
+import { useState,useEffect } from 'react';
 import BarreNavigationComponent from '../components/barreNavigation.component';
 import BarreTransactionComponent from '../components/barreTransaction.component';
 import '../App.css';
 import '../styles/accueil.page.css';
 import '../styles/transactions.css'
 export default function TransactionPage(){
+    const[transactions,setTransactions]=useState([])
+    /*
+    useEffect(()=> {
+        fetch("http://localhost:8080/forum/transactions",{
+            method:"GET"
+        }).then(res => res.json())
+          .then((result)=>{
+            setTransactions(result);
+          }
+          )
+    },[transactions])
+    */
     return(
         <div className="page_accueil">
-            <BarreNavigationComponent/>
             <BarreTransactionComponent/>
             <div className='transactions_div'>
                 <div>07/06/2023</div>
