5ffebd57e7388c77ed88cbd2ad06d2536923a004
sonko <befatou25@gmail.com>
2023-06-13 18:11:58 +0200

    refactor: mise à jour du script de creation de la table categorie

--- InflationManagementBackEnd/src/main/database/tablesCreation/CategoryTable.sql
+++ InflationManagementBackEnd/src/main/database/tablesCreation/CategoryTable.sql
@@ -3,5 +3,8 @@ DROP TABLE IF EXISTS Category;
 CREATE TABLE Category (
                           id_categorie BIGINT PRIMARY KEY AUTO_INCREMENT,
                           nom_categorie VARCHAR(255) NOT NULL,
-                          budget_categorie DOUBLE
+                          budget_categorie DOUBLE,
+                          couleur_categorie VARCHAR(255),
+                          section BIGINT,
+                          FOREIGN KEY (section) REFERENCES Section (id_section)
 );
