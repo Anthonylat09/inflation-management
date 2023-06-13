cf82bc018402affe0685bc1bb221cc7ec75b2024
sonko <befatou25@gmail.com>
2023-06-13 15:51:57 +0200

    feat: mise à jour des données de la base de données

--- InflationManagementBackEnd/src/main/database/tablesInsertion/InsertIntoCategory.sql
+++ InflationManagementBackEnd/src/main/database/tablesInsertion/InsertIntoCategory.sql
@@ -1,10 +1,7 @@
 USE budgetbd; #mettre le nom de votre bd
 
-INSERT INTO Category (nom_categorie, budget_categorie)
-VALUES ('Catégorie 1', 500.0);
-
-INSERT INTO Category (nom_categorie, budget_categorie)
-VALUES ('Catégorie 2', 1000.0);
-
-INSERT INTO Category (nom_categorie, budget_categorie)
-VALUES ('Catégorie 3', 750.0);
+INSERT INTO Category (nom_categorie, budget_categorie, couleur_categorie, section)
+VALUES
+    ('Category 1', 1000.00, 'Rouge', 1),
+    ('Category 2', 2000.00, 'Rouge', 2),
+    ('Category 3', 3000.00, 'Vert', 3);
--- InflationManagementBackEnd/src/main/database/tablesInsertion/InsertIntoTransaction.sql
+++ InflationManagementBackEnd/src/main/database/tablesInsertion/InsertIntoTransaction.sql
@@ -1,10 +1,7 @@
 USE budgetbd; #mettre le nom de votre bd
 
 INSERT INTO Transaction (nom_transaction, est_revenu, montant, categorie, date)
-VALUES ('Transaction 1', 1, 100.0, 1, '2023-06-10');
-
-INSERT INTO Transaction (nom_transaction, est_revenu, montant, categorie, date)
-VALUES ('Transaction 2', 0, 50.0, 2, '2023-06-11');
-
-INSERT INTO Transaction (nom_transaction, est_revenu, montant, categorie, date)
-VALUES ('Transaction 3', 1, 200.0, 1, '2023-06-12');
+VALUES
+    ('Transaction 1', TRUE, 100.00, 1, '2023-06-01'),
+    ('Transaction 2', FALSE, 50.00, 2, '2023-06-02'),
+    ('Transaction 3', TRUE, 200.00, 1, '2023-06-03');
