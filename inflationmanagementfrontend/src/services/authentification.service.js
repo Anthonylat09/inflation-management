665acb07fac97d962cb48a3cd7d039d30e781443
Xavier Coly <xavierfcoly@gmail.com>
2023-06-14 16:25:36 +0200

    fix: Résolution du bug sur la réponse des requêtes http dans le service d'authentification
    Se référer à ce qui est fait dans ce service pour les autres appels http

--- inflationmanagementfrontend/src/services/authentification.service.js
+++ inflationmanagementfrontend/src/services/authentification.service.js
@@ -1,28 +1,31 @@
-export async function authenticate (email,password) {
-    const user = {email,password}
+export async function authenticate(email, password) {
+    const user = {email, password}
 
-
-    fetch("http://localhost:8080/authentication/login",{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
+    fetch("http://localhost:8080/authentication/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify(user)
-    }).then(res => {if(res.status == 403) {
-        alert("Authentification échouée")
-    }
-    else {
-        localStorage.setItem('TOKEN',JSON.stringify(res.json()))
-        alert(JSON.stringify(res.json()))
-    }})
+    }).then(response => {
+        if (response.status === 403) {
+            alert("Authentification échouée")
+        } else {
+            response.json().then((result) => {
+                localStorage.setItem('TOKEN',result.authResponse);
+            });
+        }
+    })
 }
+
 
 export function getCurrentLoggedUser() {
     return fetch("http://localhost:8080/authentication/current-logged-in", {
         method: "GET",
-        headers: { Authorization: "Bearer " + localStorage.getItem('TOKEN') },
-    }).then(res => res.json());
+        headers: {Authorization: "Bearer " + localStorage.getItem('TOKEN')},
+    }).then(response =>
+        response.json().then(result => result)
+    );
 }
 
-export function isAuthenticated()
-{
-  return localStorage.getItem('TOKEN') != null
+export function isAuthenticated() {
+    return localStorage.getItem('TOKEN') != null
 }
