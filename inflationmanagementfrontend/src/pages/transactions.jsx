338ce20a9e3b1faa640ec257d73971d5da2e9149
Antoine Ndiaye <antoinendiaye09@gmail.com>
2023-06-14 13:03:42 +0200

    feat:IM-33 mise en place de la structure de la page transaction pour récuperer les transactions depuis le back et les afficher

--- inflationmanagementfrontend/src/pages/transactions.jsx
+++ inflationmanagementfrontend/src/pages/transactions.jsx
@@ -1,18 +1,22 @@
-<<<<<<< HEAD:inflationmanagementfrontend/src/pages/transactions.jsx
-=======
 import { useState,useEffect } from 'react';
-import BarreNavigationComponent from '../components/barreNavigation.component';
->>>>>>> c969f79 (feat:IM-33 ajout du code de base pour recuperer les transactions du backend):inflationmanagementfrontend/src/pages/transactions.js
 import BarreTransactionComponent from '../components/barreTransaction.component';
 import '../App.css';
 import '../styles/accueil.page.css';
 import '../styles/transactions.css'
-
 export default function TransactionPage(){
     const[transactions,setTransactions]=useState([])
-    /*
+    
     useEffect(()=> {
-        fetch("http://localhost:8080/forum/transactions",{
+        const currentDate = new Date();
+        const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
+        const startDateISO = startOfMonth.toISOString().split('T')[0];
+        const endDateISO = endOfMonth.toISOString().split('T')[0];
+
+        const url = `http://localhost:8080/forum/transactions-between-dates?startDate=${startDateISO}&endDate=${endDateISO}`;
+
+        fetch(url,{
             method:"GET"
         }).then(res => res.json())
           .then((result)=>{
@@ -20,14 +24,20 @@ export default function TransactionPage(){
           }
           )
     },[transactions])
-    */
+    
     return(
         <div className="page_accueil">
             <BarreTransactionComponent/>
-            <div className='transactions_div'>
-                <div>07/06/2023</div>
-                <div className='long_separator'></div>
-                <div className='circle blue_circle'></div>
+            <div>
+            {transactions.map(transaction=>(
+                
+                <div className='transactions_div'>
+                    <div>{transaction.dateTransaction}</div>
+                    <div className='long_separator'></div>
+                    <div className='circle blue_circle'></div>
+                    {transaction.nomTransaction}
+                </div>
+            ))}
             </div>
             <button className='ajout_transaction_button'>Ajouter une transaction</button>
         </div>
