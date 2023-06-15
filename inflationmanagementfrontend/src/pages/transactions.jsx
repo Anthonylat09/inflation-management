27aea89dd99e2a433b411b64eaa839b2cc03e3f3
Antoine Ndiaye <antoinendiaye09@gmail.com>
2023-06-15 16:28:43 +0200

    feat:IM-33 suppression du fichier barreTransaction et refactorisation pui ajout de la possibilité de changer de mois dans la page transactions et de mettre à jour la liste des transactions en conséquence

--- inflationmanagementfrontend/src/pages/transactions.jsx
+++ inflationmanagementfrontend/src/pages/transactions.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import BarreTransactionComponent from "../components/barreTransaction.component";
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from 'react-icons/ai';
+import '../styles/barreTransaction.component.css'
 import "../App.css";
 import "../styles/accueil.page.css";
 import "../styles/transactions.css";
@@ -7,17 +8,118 @@ import { getTransactions } from "../services/transaction.service";
 export default function TransactionPage() {
   const [transactions, setTransactions] = useState([]);
 
-  const currentDate = new Date();
-  const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-  const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  const [currentDate, setCurrentDate] = useState(new Date())
+  const [monthYear, setMonthYear] = useState(currentDate.toLocaleString('default', { month: 'long', year: 'numeric' }));
 
+  const [startOfMonth, setStartOfMonth] = useState(new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  ))
+  const [endOfMonth, setEndOfMonth] = useState(new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  ))
+
+  function handleLeftButtonClick(e) {
+    e.preventDefault();
+    
+    // Create a new Date object based on the current month
+    const currentMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+
+    // Subtract one month from the current month
+    currentMonthDate.setMonth(currentMonthDate.getMonth() - 1);
+
+    // Update the currentDate state with the new date
+    setCurrentDate(currentMonthDate);
+
+    setStartOfMonth(new Date(
+      currentMonthDate.getFullYear(),
+      currentMonthDate.getMonth(),
+      0
+    ))
+
+    setEndOfMonth(new Date(
+      currentMonthDate.getFullYear(),
+      currentMonthDate.getMonth() + 1,
+      0
+    ))
+
+    // Update the monthYear state with the new month and year
+    setMonthYear(currentMonthDate.toLocaleString('default', { month: 'long', year: 'numeric' }));
+    
+  }
+
+  function handleRightButtonClick(e) {
+    e.preventDefault();
+
+    // Create a new Date object based on the current month
+    const currentMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+
+    // Subtract one month from the current month
+    currentMonthDate.setMonth(currentMonthDate.getMonth() + 1);
+
+    // Update the currentDate state with the new date
+    setCurrentDate(currentMonthDate);
+
+    setStartOfMonth(new Date(
+      currentMonthDate.getFullYear(),
+      currentMonthDate.getMonth(),
+      0
+    ))
+
+    setEndOfMonth(new Date(
+      currentMonthDate.getFullYear(),
+      currentMonthDate.getMonth() + 1,
+      0
+    ))
+
+    // Update the monthYear state with the new month and year
+    setMonthYear(currentMonthDate.toLocaleString('default', { month: 'long', year: 'numeric' }));
+  }
   useEffect(() => {
     getTransactions(startOfMonth, endOfMonth).then((tr) => setTransactions(tr));
-  }, []);
+  }, [startOfMonth, endOfMonth]);
 
   return (
     <div className="page_accueil">
-      <BarreTransactionComponent />
+      <div className="barreTr_div">
+        <div className="nombres_div">
+          <div className="revenus_div">
+            <span>
+              Revenus <br />
+              {}
+            </span>
+          </div>
+          <div className="depenses_div">
+            <span>
+              Dépenses
+              <br />
+              {}
+            </span>
+          </div>
+          <div className="solde_div">
+            <span>
+              Solde
+              <br />
+              {}
+            </span>
+          </div>
+        </div>
+        <div className="mois_div">
+          <button id="left_button" onClick={handleLeftButtonClick}>
+            <AiOutlineArrowLeft />
+          </button>
+          <span>
+            {monthYear}
+            <br />{transactions.length} transacions
+          </span>
+          <button id="right_button" onClick={handleRightButtonClick}>
+            <AiOutlineArrowRight />
+          </button>
+        </div>
+      </div>
       <div>
         {transactions.map((transaction) => (
           <div className="transactions_div">
