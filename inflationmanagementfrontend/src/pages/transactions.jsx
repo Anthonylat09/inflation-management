a4f798eb705815eff90f4358325d10b6be8cc325
Antoine Ndiaye <antoinendiaye09@gmail.com>
2023-06-15 15:31:49 +0200

    refactor:IM-23 réécriture du fichier transactions.jsx

--- inflationmanagementfrontend/src/pages/transactions.jsx
+++ inflationmanagementfrontend/src/pages/transactions.jsx
@@ -1,40 +1,46 @@
-import { useState,useEffect } from 'react';
-import BarreTransactionComponent from '../components/barreTransaction.component';
-import '../App.css';
-import '../styles/accueil.page.css';
-import '../styles/transactions.css'
-import { getTransactions } from '../services/transaction.service';
-export default function TransactionPage(){
-  const [tr, setTr] = useState([]);
+import { useState, useEffect } from "react";
+import BarreTransactionComponent from "../components/barreTransaction.component";
+import "../App.css";
+import "../styles/accueil.page.css";
+import "../styles/transactions.css";
+import { getTransactions } from "../services/transaction.service";
+export default function TransactionPage() {
+  const [transactions, setTransactions] = useState([]);
 
   const currentDate = new Date();
-  const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-  const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  const startOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+  const endOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  );
 
-  useEffect(()=>{
-    getTransactions(startOfMonth, endOfMonth).then(transactions=>setTr(transactions));
-  },[])
+  useEffect(() => {
+    getTransactions(startOfMonth, endOfMonth).then((tr) => setTransactions(tr));
+  }, []);
 
- 
-    return(
-        <div className="page_accueil">
-            <BarreTransactionComponent/>
-            <div>
-              {
-                  tr.map((transaction => 
-                  <div className="transactions_div">
-                    <div>{transaction.dateTransaction}</div>
-                    <div className="long_separator"></div>
-                    <div style={{display: 'flex'}}>
-                      <div className="circle blue_circle"></div>
-                      <div>{transaction.nomTransaction}</div>
-                    </div>   
-                  </div>
-              ))}             
-            
+  return (
+    <div className="page_accueil">
+      <BarreTransactionComponent />
+      <div>
+        {transactions.map((transaction) => (
+          <div className="transactions_div">
+            <div>{transaction.dateTransaction}</div>
+            <div className="long_separator"></div>
+            <div style={{ display: "flex" }}>
+              <div className="circle blue_circle"></div>
+              <div>{transaction.nomTransaction}</div>
             </div>
-            <button className='ajout_transaction_button'>Ajouter une transaction</button>
-        </div>
-        
-    );
+          </div>
+        ))}
+      </div>
+      <button className="ajout_transaction_button">
+        Ajouter une transaction
+      </button>
+    </div>
+  );
 }
