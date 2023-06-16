d46ae332374aadb68bc9c0a06ac4790d9771ca74
Xavier Coly <xavierfcoly@gmail.com>
2023-06-17 00:02:35 +0200

    feat: Adaptation de la partie graphique en fonction des données

--- inflationmanagementfrontend/src/pages/budget.page.jsx
+++ inflationmanagementfrontend/src/pages/budget.page.jsx
@@ -2,49 +2,75 @@ import PaginationComponent from '../components/pagination.component';
 import CarteSectionComponent from '../components/carteSection.component';
 import '../styles/budget.page.css';
 import PieChartUtils from '../utils/pieChart.utils';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import {getAllSectionsWithCategories, getPieChartData} from '../services/budget.service';
 
 export default function BudgetPage() {
     const [isProgrammerButtonActive, activeProgrammerButton] = useState(true);
-    const [sections, setSections] = useState([])
+    const [sections, setSections] = useState([]);
+    const [totalBudget, setTotalBudget] = useState(0.);
+    const [data, setData] = useState({});
+    const [totalRestant, setTotalRestant] = useState(0.);
+    useEffect(() => {
+        getPieChartData().then(data => setData(data));
+        getAllSectionsWithCategories().then(sections => {
+            // Set the sections
+            setSections(sections);
+            //Set totalBudget
+            let value = sections.reduce((previousValue, currentValue) => {
+                previousValue += currentValue.budgetTotal;
+                return previousValue;
+            }, 0.);
+            setTotalBudget(value);
+            //Set totalRestant
+            let rest = sections.reduce((acc, current) => {
+                let sum = 0.;
+                for (const categoryListElement of current.categoryList) {
+                    sum += categoryListElement.budgetRestant;
+                }
+                acc += sum;
+                return acc;
+            }, 0.)
+            setTotalRestant(rest);
+        });
+    }, [])
     return (
         <div>
-            <PaginationComponent></PaginationComponent>
+            <PaginationComponent date={new Date().toLocaleDateString('fr')}></PaginationComponent>
             <div id="programmer_restant">
                 <button className={isProgrammerButtonActive ? 'orange_button' : ''} onClick={() => {
                     activeProgrammerButton(true)
                 }}>Programmer
                 </button>
-                <button className={isProgrammerButtonActive ? '' : 'orange_button'} onClick={()=>{activeProgrammerButton(false)}}>Restant</button>
+                <button className={isProgrammerButtonActive ? '' : 'orange_button'} onClick={() => {
+                    activeProgrammerButton(false)
+                }}>Restant
+                </button>
             </div>
             <section className={isProgrammerButtonActive ? 'budgetSection' : 'hide'}>
                 <div id="chart_section">
                     <div id="chart">
-                        <PieChartUtils series={[44, 55, 13, 43]}></PieChartUtils>
+                        {data.series ? <PieChartUtils data={data}></PieChartUtils> : ''}
                     </div>
                     <div id="side_text">
                         <div>Total des dépenses programmées</div>
-                        <div>850 €</div>
-                        <div>Budget restant: 290€</div>
+                        <div>{totalBudget + ' €'}</div>
+                        <div>Budget restant: {totalRestant} €</div>
                     </div>
                 </div>
                 <div id="categ_section">
-                    <div className="square red_circle"></div>
-                    <div>Logement</div>
-                    <div className="pointille"></div>
-                    <div>50€</div>
-                    <div className="square green_circle"></div>
-                    <div>Binks</div>
-                    <div className="pointille"></div>
-                    <div>50€</div>
-                    <div className="square orange_circle"></div>
-                    <div>Hola</div>
-                    <div className="pointille"></div>
-                    <div>50€</div>
-                    <div className="square blue_circle"></div>
-                    <div>Nourriture</div>
-                    <div className="pointille"></div>
-                    <div>50€</div>
+                    {
+                        sections.map(section => {
+                            return (
+                                <>
+                                    <div className={`square ${section.couleurSection}_circle`}></div>
+                                    <div>{section.nomSection}</div>
+                                    <div className="pointille"></div>
+                                    <div>{section.budgetTotal}</div>
+                                </>
+                            );
+                        })
+                    }
                 </div>
             </section>
             <div className={"categories"}>
