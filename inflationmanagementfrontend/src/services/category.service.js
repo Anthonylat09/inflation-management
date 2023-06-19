e894a39f72c422816de619acc30fa79d298c29a2
Xavier Coly <xavierfcoly@gmail.com>
2023-06-19 19:28:50 +0200

    fix: Reglage de l'état de la barre de navigation quand l'utilisateur est connecté ou non

--- inflationmanagementfrontend/src/services/category.service.js
+++ inflationmanagementfrontend/src/services/category.service.js
@@ -1,10 +1,10 @@
-const url = "http://localhost:8080/categories/";
+const url = "http://localhost:8080/categories";
 
 export async function createCategory(category){
     return fetch(`${url}`,{
         method: "POST",
         headers: {Authorization: "Bearer " + localStorage.getItem('TOKEN')},
-        body: JSON.stringify(category)
+        body: category
     }).then(response =>
         response.json().then(result => result)
     );
